ad1e2ce608c6032deadfa9fc2c4bcf4f24a83e08
awasson1 <47997743+awasson1@users.noreply.github.com>
2023-05-31 12:33:45 -0700

    Project Creation
    skeleton project to begin working on

---
+++ weSplit/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Anthony Wasson on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
