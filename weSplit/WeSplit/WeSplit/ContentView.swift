d3408aeae3f2d94f44798a0f3f0e6acf05421dad
awasson1 <47997743+awasson1@users.noreply.github.com>
2023-05-31 22:30:36 -0700

    foreach loop test

--- weSplit/WeSplit/WeSplit/ContentView.swift
+++ weSplit/WeSplit/WeSplit/ContentView.swift
@@ -8,19 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = "";
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form
+        {
+            ForEach(0..<100)
+            {
+                //number in
+                Text("Row \($0)")
+            }
         }
-        .padding()
     }
 }
 
+/*
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+*/
