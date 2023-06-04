056a0b86ecc0885225fcdb7c4e3038630610b2eb
awasson1 <47997743+awasson1@users.noreply.github.com>
2023-06-03 23:17:39 -0700

    Total per person calculation and display
    added functionality to calculate the total cost per person dependent on the total cost, and the tip added, then split between the number of people given. This number of cost per person is then displayed at the bottom

--- weSplit/WeSplit/WeSplit/ContentView.swift
+++ weSplit/WeSplit/WeSplit/ContentView.swift
@@ -7,14 +7,28 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View
+{
     
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     let tipPercentages = [10, 15, 20, 25, 0]
 
-    var body: some View {
+    var totalPerPerson: Double
+    {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View
+    {
         NavigationView
         {
             Form
@@ -35,9 +49,26 @@ struct ContentView: View {
                 
                 Section
                 {
-                    Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    Picker("Tip percentage", selection: $tipPercentage)
+                    {
+                        ForEach(tipPercentages, id: \.self)
+                        {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                    header:
+                    {
+                        Text("How much tip would you like to leave?")
+                    }
+                
+                Section
+                {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                 }
             }
+            .navigationTitle("WeSplit")
         }
     }
 }
